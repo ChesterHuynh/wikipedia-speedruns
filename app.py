21851f2509273aee207e4ffb816573d275a0f880
Dan Qian <daniel.j.qian@gmail.com>
2022-03-22 01:52:24 -0400

    get celery working

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import json
 import db
 import mail
 import tokens
+import tasks
+
 from util.flaskjson import ISODateJSONEncoder
 
 import sentry_sdk
@@ -42,6 +44,7 @@ def create_app(test_config=None):
     db.init_app(app)
     mail.init_app(app)
     tokens.init_app(app)
+    tasks.make_celery(app)
 
     from apis.sprints import sprint_api
     from apis.runs import run_api
